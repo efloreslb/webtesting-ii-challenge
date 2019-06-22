4a53ef0d6119060ec003595be5962d0f5e684356
Edgar F <eflores.ef@gmail.com>
2019-06-22 11:57:15 -0700

    initialized react app

---
+++ baseball/src/components/Display.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Display extends React.Component {
+   state = {
+
+   }
+
+   render() {
+      return (
+         <>
+            <h1>Display</h1>
+         </>
+      )
+   }
+}
+
+export default Display;
